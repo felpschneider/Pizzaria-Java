990db1c48c0199e8cea6c45c6f8a12d821422438
Felipe Schneider <felipeschneider2811@gmail.com>
2022-06-14 21:41:31 -0300

    Versão Alpha 1.0

---
+++ Projeto do bloco 1/src/org/generation/brazil/EntregarRetirar.java
@@ -0,0 +1,23 @@
+package org.generation.brazil;
+
+public class EntregarRetirar{
+
+public void Finalizar() throws InterruptedException {
+	
+	System.out.println("Seu pedido foi feito com sucesso!");
+
+	for(int i = 0; i <= 2; i++){
+	System.out.println(".");
+	Thread.sleep(500);
+	}
+	
+	System.out.println("Seu pedido está sendo preparado");
+	for(int i = 0; i <= 3; i++){
+	System.out.println(".");
+	Thread.sleep(1500);
+	}
+	System.out.println("Seu pedido saiu para entrega ou está pronto para ser retirado");
+	System.out.println("Obrigado por realizar seu pedido na Java's Pizzaria! Até logo!");
+
+}
+}
